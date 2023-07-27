b59e1f684032a693f4d98aa5e96e0879b4192d9e
Sleepingoff <justjh30@gmail.com>
2023-07-27 18:16:36 +0900

    [UPDATE] 상 하단 고정

--- 이지현_bucketlist/fullscreen.js
+++ 이지현_bucketlist/fullscreen.js
@@ -1,16 +1,17 @@
 import { bucketForm, bucketList, buckets } from "./index.js";
-const section = document.querySelector("section");
+const section = document.querySelector(".section");
 const header = document.querySelector(".header");
+/*
+form이 빠져나오는 순간 height가 줄어들면서 과부하가 옴...
+
 const resizeOb = new ResizeObserver((entries) => {
   for (let entry of entries) {
     const { height } = entry.contentRect;
     if (height >= window.innerHeight) {
-      header.classList.add("full-screen");
-      bucketForm.classList.add("full-screen");
-    } else {
-      header.classList.remove("full-screen");
-      bucketForm.classList.remove("full-screen");
+      header.classList.toggle("full-screen");
+      bucketForm.classList.toggle("full-screen");
     }
   }
 });
 resizeOb.observe(section);
+*/
